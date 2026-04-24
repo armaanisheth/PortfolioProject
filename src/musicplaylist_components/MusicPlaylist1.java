4d3cc7952622222bece7ba9d6ac9fbf9879b506a
armaanisheth <armaanisheth@gmail.com>
2026-04-24 17:12:25 -0400

    Update MusicPlaylist1.java

--- src/musicplaylist_components/MusicPlaylist1.java
+++ src/musicplaylist_components/MusicPlaylist1.java
@@ -1,5 +1,71 @@
 package musicplaylist_components;
 
-public class MusicPlaylist1 {
+import components.map.Map;
+import components.map.Map.Pair;
+import components.map.Map1L;
 
+/**
+ * @convention
+ * [$this.playlist is not null]
+ * [every String key contained in this.playlist is not ""]
+ * [every String value contained in this.playlist is not ""]
+ *
+ * @correspondance
+ * [this.playlist is a map with at least one pair with String key and String value]
+ * @author Armaani Sheth.116
+ */
+public class MusicPlaylist1 extends MusicPlaylistSecondary {
+
+    /*
+     * Private members --------------------------------------------------------
+     */
+
+    /**
+     * Representation of the current playlist.
+     */
+    private Map<String, String> playlist;
+
+    /**
+     * Creator of initial representation.
+     */
+    private void createNewRep() {
+        this.playlist = new Map1L<String, String>();
+    }
+
+    /*
+     * Constructors -----------------------------------------------------------
+     */
+
+    /**
+     * No-argument constructor.
+     */
+    public MusicPlaylist1() {
+        this.createNewRep();
+    }
+
+    /*
+     * Kernel methods ---------------------------------------------------------
+     */
+
+    @Override
+    public void add(String artist, String song) {
+        this.playlist.add(song, artist);
+    }
+
+    @Override
+    public Map.Pair<String, String> remove(String song) {
+        return this.playlist.remove(song);
+    }
+
+    @Override
+    public int length() {
+        return this.playlist.size();
+    }
+
+    @Override
+    public final Pair<String, String> removeAny() {
+        assert this.playlist.size() > 0 : "Violation of: this /= empty_set";
+        return this.playlist.removeAny();
+    }
 }
+
